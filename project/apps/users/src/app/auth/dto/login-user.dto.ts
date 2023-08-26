7714eae29d3eac6ae2558d7fbcfcee5c8fc7268d
squbert <s.ssqubert@gmail.com>
2023-08-26 23:19:31 +0300

    update user

--- project/apps/users/src/app/auth/dto/login-user.dto.ts
+++ project/apps/users/src/app/auth/dto/login-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
 import { AUTH_USER_EMAIL_NOT_VALID } from '../auth.constant';
 
 export class LoginUserDTO {
@@ -15,5 +15,6 @@ export class LoginUserDTO {
     example: 'qwerty',
   })
   @IsString()
+  @Length(6, 12, { message: 'Min length for password is 6, max is 12' })
   public password: string;
 }
